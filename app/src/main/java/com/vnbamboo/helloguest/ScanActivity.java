d9e1e63cd578edcdcf35d07b52b3b557b8d9e4df
Ống Tre <tamdaulong207@gmail.com>
2019-02-15 20:45:55 +0700

    no message

--- app/src/main/java/com/vnbamboo/helloguest/ScanActivity.java
+++ app/src/main/java/com/vnbamboo/helloguest/ScanActivity.java
@@ -6,9 +6,11 @@ import android.util.SparseArray;
 import android.widget.Toast;
 
 import com.google.android.gms.vision.barcode.Barcode;
+import com.vnbamboo.helloguest.camera.GraphicOverlay;
 
 import java.util.List;
 
+import info.androidhive.barcode.BarcodeGraphic;
 import info.androidhive.barcode.BarcodeReader;
 
 public class ScanActivity extends AppCompatActivity implements BarcodeReader.BarcodeReaderListener {
@@ -49,5 +51,40 @@ public class ScanActivity extends AppCompatActivity implements BarcodeReader.Bar
     @Override
     public void onScanError(String s) {
         Toast.makeText(getApplicationContext(), "Error occurred while scanning " + s, Toast.LENGTH_SHORT).show();
+    }
+    private boolean onTap(float rawX, float rawY) {
+        // Find tap point in preview frame coordinates.
+        int[] location = new int[2];
+
+        mGraphicOverlay.getLocationOnScreen(location);
+        float x = (rawX - location[0]) / mGraphicOverlay.getWidthScaleFactor();
+        float y = (rawY - location[1]) / mGraphicOverlay.getHeightScaleFactor();
+
+        // Find the barcode whose center is closest to the tapped point.
+        Barcode best = null;
+        float bestDistance = Float.MAX_VALUE;
+        for (BarcodeGraphic graphic : mGraphicOverlay.getGraphics()) {
+            Barcode barcode = graphic.getBarcode();
+            if (barcode.getBoundingBox().contains((int) x, (int) y)) {
+                // Exact hit, no need to keep looking.
+                best = barcode;
+                break;
+            }
+            float dx = x - barcode.getBoundingBox().centerX();
+            float dy = y - barcode.getBoundingBox().centerY();
+            float distance = (dx * dx) + (dy * dy);  // actually squared distance
+            if (distance < bestDistance) {
+                best = barcode;
+                bestDistance = distance;
+            }
+        }
+        //barcode scan success full
+        if (best != null) {
+            Intent data = new Intent();
+            //setResult(CommonStatusCodes.SUCCESS, data);
+            //finish();
+            return true;
+        }
+        return false;
     }
 }
